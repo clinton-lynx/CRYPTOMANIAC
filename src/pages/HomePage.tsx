33367ca36afb94b8924edafd06b3f6573ffdccbc
clinton-lynx <clintonadeoti1@gmail.com>
2024-08-21 00:00:41 +0100

    add

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -10,6 +10,7 @@ import BlogCard from "../components/BlogCard";
 import millify from "millify";
 import moment from "moment";
 import Preloader from "../components/preloader";
+import Pagination from "../components/Pagination";
 
 const HomePage = () => {
   const NEWS_API_KEY: any = process.env.NEWS_API_KEY
@@ -17,7 +18,8 @@ const HomePage = () => {
   const [loading, setLoading] =useState(true)
   const [trendCoins, setTrendingCoins] = useState([]);
   const [news, setNews]: any = useState([])
-
+  
+  
   const [search, setSearch] = useState("");
   const [slide, setSlide] = useState(false);
   const currentSlide = slide ? "translateX(1000px)" : "translateX(0px)";
@@ -29,7 +31,7 @@ const HomePage = () => {
   const fetchData = async () => {
     try {
       const response = await axios(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=27&page=1&sparkline=false"
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&page=1&sparkline=false"
       );
       console.log(response);
       const data = response.data;
@@ -48,7 +50,7 @@ const HomePage = () => {
   const filteredNews = news.filter((news: any) =>
     news.name.toLowerCase().includes(search.toLowerCase())
   );
-
+  
 
   const options = {
     method: 'GET',
@@ -77,7 +79,7 @@ const HomePage = () => {
 
   }, [])
 
-
+  
   const fetchTrendingData = async () => {
     try {
       const response = await axios(
@@ -87,7 +89,7 @@ const HomePage = () => {
       console.log(response.data.coins);
     } catch (error) { }
   };
-
+  
   const scrollBack = () => {
     const carousel = document.querySelector(".crypto-card-wrapper")!;
     carousel.scrollLeft = carousel.scrollLeft - 300;
@@ -104,17 +106,11 @@ const HomePage = () => {
   useEffect(() => {
     fetchData();
     fetchTrendingData();
-   
+    
   }, []);
 
 
-  const filteredCoins = coins.filter((coin: any) =>
-    coin.name.toLowerCase().includes(search.toLowerCase())
-  );
-  console.log(filteredCoins);
-  console.log(coins);
-  console.log(search);
-
+  
   const mobileNavSlide = () => {
     setSlide((prev) => !prev);
     const mobileNav = document.querySelector(".mobile-menu") as HTMLElement;
@@ -125,7 +121,7 @@ const HomePage = () => {
     console.log("slide werey");
     console.log(slide);
   };
-
+  
   // TRENDS SIDE EFFECT WHEN SEARCHING
   const searchSideEffect = () => {
     const trendSection = document.querySelector(".trends") as HTMLElement;
@@ -140,6 +136,20 @@ const HomePage = () => {
     trendSection.style.marginTop = "3rem";
     console.log("now on blur");
   };
+
+  const filteredCoins = coins.filter((coin: any) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+);
+console.log(filteredCoins);
+  console.log(coins);
+  console.log(search);
+  
+
+  const [currentPage, setCurrentPage] = useState(5);
+  const postPerPage = 20;
+  const indexOfLastPost = currentPage * postPerPage;
+  const indexOfFirstPost = indexOfLastPost - postPerPage;
+  const newCoins = filteredCoins.slice( indexOfFirstPost , indexOfLastPost );
 
   return (
     <>
@@ -244,7 +254,7 @@ const HomePage = () => {
             <Preloader /> 
           ): (
             <div className="crypto-card-wrapper--today">
-              {filteredCoins.map((coin: any) => (
+              {newCoins.map((coin: any) => (
                 <CryptoCard
                   coinId={coin.id}
                   name={coin.name}
@@ -260,6 +270,9 @@ const HomePage = () => {
               ))}
             </div>
           )}
+          <div className="pagination-wrapper">
+            <Pagination />
+          </div>
           </section>
           <div className="bottom-section">
             <section className="sections-wrapper">
